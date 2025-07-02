faa5da755f8be2197829292f9320c056f6aaf928
Guilherme Teixeira <24235344+ggteixeira@users.noreply.github.com>
2025-07-01 22:23:35 -0300

    Creates a first Controller with a post request

---
+++ dotnet-alura/FilmesApi/Controllers/FilmeController.cs
@@ -0,0 +1,21 @@
+using FilmesApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.WebUtilities;
+
+namespace FilmesApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FilmeController: ControllerBase
+{
+    private static List<Filme> filmes = new List<Filme>();
+    
+    [HttpPost]
+    public void AdicionaFilme([FromBody] Filme filme)
+    {
+        filmes.Add(filme);
+        Console.WriteLine($"Filme adicionado com sucesso!");
+        Console.WriteLine(filme.Titulo);
+        Console.WriteLine(filme.Genero);
+    }
+}
