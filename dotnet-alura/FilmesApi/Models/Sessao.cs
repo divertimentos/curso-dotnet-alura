3d9778b9f6ccdf1f287bf8eb3587aafcc8379de0
Guilherme Teixeira <24235344+ggteixeira@users.noreply.github.com>
2025-08-10 22:56:04 -0300

    Dropa a database, roda migration, atualiza Model pra permitir filme e
    cinema nulos

--- dotnet-alura/FilmesApi/Models/Sessao.cs
+++ dotnet-alura/FilmesApi/Models/Sessao.cs
@@ -4,9 +4,9 @@ namespace FilmesApi.Models;
 
 public class Sessao
 {
-    [Key] [Required] public int Id { get; set; }
+    [Key][Required] public int Id { get; set; }
 
-    [Required] public int FilmeId { get; set; }
+    public int? FilmeId { get; set; }
 
     public virtual Filme Filme { get; set; }
 
