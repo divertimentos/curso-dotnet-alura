be383b5973ca29807638f39c3a2f534d58b9759f
Guilherme Teixeira <24235344+ggteixeira@users.noreply.github.com>
2025-08-10 23:14:47 -0300

    Relaç. n:n e o professor rushou o curso do nada

--- dotnet-alura/FilmesApi/Models/Sessao.cs
+++ dotnet-alura/FilmesApi/Models/Sessao.cs
@@ -4,8 +4,6 @@ namespace FilmesApi.Models;
 
 public class Sessao
 {
-    [Key][Required] public int Id { get; set; }
-
     public int? FilmeId { get; set; }
 
     public virtual Filme Filme { get; set; }
