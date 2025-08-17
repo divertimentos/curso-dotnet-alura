787f901178e9958b1f74f9312be589b9059d6cc2
Guilherme Teixeira <24235344+ggteixeira@users.noreply.github.com>
2025-08-16 21:57:58 -0300

    Redoes linq

--- dotnet-alura/FilmesApi/Controllers/FilmeController.cs
+++ dotnet-alura/FilmesApi/Controllers/FilmeController.cs
@@ -39,19 +39,9 @@ public class FilmeController : ControllerBase
     }
 
     [HttpGet]
-    public IEnumerable<ReadFilmeDto> RecuperaFilmes(
-        [FromQuery] int skip = 0,
-        [FromQuery] int take = 5,
-        [FromQuery] string? nomeDoCinema = null
-    )
+    public IEnumerable<ReadFilmeDto> RecuperaFilmes([FromQuery] int skip = 0, [FromQuery] int take = 5)
     {
-        if (nomeDoCinema == null)
-        {
-            return _mapper.Map<List<ReadFilmeDto>>(_context.Filmes.Skip(skip).Take(take).ToList());
-        }
-
-        return _mapper.Map<List<ReadFilmeDto>>(_context.Filmes.Skip(skip).Take(take)
-            .Where(filme => filme.Sessoes.Any(sessao => sessao.Cinema.Nome == nomeDoCinema)).ToList());
+        return _mapper.Map<List<ReadFilmeDto>>(_context.Filmes.Skip(skip).Take(take).ToList());
     }
 
     [HttpGet("{id}")]
